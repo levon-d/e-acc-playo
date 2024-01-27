512bb3170f88b7193d9c701300376f8012f2eb8b
Raghav <raghavawasthi2005@gmail.com>
2024-01-27 20:30:56 +0000

    added firestore database & firebase storage

--- functions/main.py
+++ functions/main.py
@@ -2,18 +2,39 @@
 # To get started, simply uncomment the below code or create your own.
 # Deploy with `firebase deploy`
 
-from firebase_functions import https_fn
-from firebase_admin import initialize_app, firestore, db, storage
+import firebase_admin
+import google.cloud
 import google.cloud.firestore
-from openai import OpenAI
-import settings as settings
-from elevenlabs import generate, voices
 
-app = initialize_app()
+# import settings as settings
+from firebase_functions import https_fn
+from firebase_admin import initialize_app, credentials, firestore, storage
+from openai import OpenAI
+from elevenlabs import voices, generate
+
 chat_gpt_client = OpenAI()
 
+# firestore database
+cred = credentials.Certificate("./ServiceAccountKey.json")
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
+doc_ref = db.collection("stories")
+# doc_ref = db.collection("stories").limit(2)
+try:
+    docs = doc_ref.get()
+    for doc in docs:
+        print("Doc Data:{}".format(doc.to_dict()))
+except google.cloud.exceptions.NotFound:
+    print("Missing data")
 
-# generate story from inputs
+# firebase storage
+bucket = storage.bucket()
+
+# firebase app
+app = initialize_app(cred, {"storageBucket": "playo-913ea.appspot.com"})
+
+
+# generate story from inputs and return json of story & character descriptions
 @https_fn.on_request()
 def generate_story(req: https_fn.Request) -> https_fn.Response:
     data = req.get_json()
@@ -37,23 +58,21 @@ def generate_story(req: https_fn.Request) -> https_fn.Response:
     response_character = generate_characters_json(response_story)
     print(response_story)
     print(response_character)
-    
-    response_object = {
-        "story": response_story,
-        "characters": response_character
-    }
 
-    return https_fn.Response(response_object)
+    response_object = {"story": response_story, "characters": response_character}
 
+    return https_fn.Response.json(response_object)
+
+
+# generate character descriptions from story
 def generate_characters_json(story: str) -> str:
-
     completion = chat_gpt_client.chat.completions.create(
         model="gpt-4",
         messages=[
             {"role": "system", "content": "You are a world class story writer/author."},
             {
                 "role": "user",
-                "content": f"given the following story, give a general description of each character, as well as their age and gender. Format the response as a list JSON. The list should have the following structure: \n [ \n {{ \n \"name\": ..., \n \"description\": ..., \n \"voice_description\": ..., \n \"age\": ..., \n \"gender\": ... \n }}, \n ... \n ] \n\n Here is the story: \"{story}\"",
+                "content": f'given the following story, give a general description of each character, as well as their age and gender. Format the response as a list JSON. The list should have the following structure: \n [ \n {{ \n "name": ..., \n "description": ..., \n "voice_description": ..., \n "age": ..., \n "gender": ... \n }}, \n ... \n ] \n\n Here is the story: "{story}"',
             },
         ],
     )
@@ -61,6 +80,7 @@ def generate_characters_json(story: str) -> str:
     response = completion.choices[0].message.content
 
     return response
+
 
 @https_fn.on_request()
 def generate_narration(req: https_fn.Request) -> https_fn.Response:
@@ -70,17 +90,11 @@ def generate_narration(req: https_fn.Request) -> https_fn.Response:
     document_id = data.get("document_id")
     firestore_client: google.cloud.firestore.Client = firestore.client()
     story = firestore_client.collection("stories").document(document_id).get().to_dict()
-    audio = generate(
-        text=story.story,
-        voice=voice_id
-    )
+    audio = generate(text=story.story, voice=voice_id)
     audio_data = audio.data
 
     # upload to firebase storage
-    bucket = storage.bucket()
     blob = bucket.blob(f"{story.id}/{document_id}.mp3")
     blob.upload_from_string(audio_data, content_type="audio/mpeg")
 
     print("Audio file successfully uploaded to firebase storage")
-
-    
