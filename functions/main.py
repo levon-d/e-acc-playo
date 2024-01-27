c60b958f66fe4ad4825446fec1965c4b4467a2e7
Raghav <raghavawasthi2005@gmail.com>
2024-01-27 18:20:13 +0000

    added characters' descriptions

--- functions/main.py
+++ functions/main.py
@@ -15,11 +15,11 @@ chat_gpt_client = OpenAI()
 # generate story from inputs
 @https_fn.on_request()
 def generate_story(req: https_fn.Request) -> https_fn.Response:
-    text = req.args.get("text")
-    theme = req.args.get("theme")
-    age_rating = req.args.get("age_rating")
-    # story_line = req.args.get("storyLine")
-    word_count = req.args.get("word_count")
+    data = req.get_json()
+    text = data.get("text")
+    theme = data.get("theme")
+    age_rating = data.get("age_rating")
+    word_count = data.get("word_count")
 
     completion = chat_gpt_client.chat.completions.create(
         model="gpt-4",
@@ -31,6 +31,27 @@ def generate_story(req: https_fn.Request) -> https_fn.Response:
             },
         ],
     )
+
+    response = completion.choices[0].message
+    print(completion.choices[0].message)
+
+    return https_fn.Response(response.content)
+
+def generate_characters_json(req: https_fn.Request) -> https_fn.Response:
+    data = req.get_json()
+    story = data.get("story")
+
+    completion = chat_gpt_client.chat.completions.create(
+        model="gpt-4",
+        messages=[
+            {"role": "system", "content": "You are a world class story writer/author."},
+            {
+                "role": "user",
+                "content": f"given the following story, give a general description of each character, as well as their age and gender. Format the response as a JSON. Here is the story: \"{story}\"",
+            },
+        ],
+    )
+
     response = completion.choices[0].message
     print(completion.choices[0].message)
 
